5cbd751e27099f063c93806d3ad5736b5df4ab52
rudrax mehta <mr.rudraxmehta@gmail.com>
2023-05-28 22:40:48 +0530

    up

--- src/views/createpage/dynamic/index.js
+++ src/views/createpage/dynamic/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react"
-import { Button, Card, CardBody, Col, Form, Label, Row } from "reactstrap"
+import { BreadcrumbItem, Button, Card, CardBody, Col, Form, Label, Row } from "reactstrap"
 import { Editor } from 'react-draft-wysiwyg'
 import '@styles/react/libs/editor/editor.scss'
 import '@styles/react/libs/react-select/_react-select.scss'
@@ -11,7 +11,8 @@ import { Check } from "react-feather"
 import Breadcrumbs from '@components/breadcrumbs'
 import Avatar from '@components/avatar'
 import { useParams } from 'react-router'
-
+import { Link } from "react-router-dom/cjs/react-router-dom"
+Link
 const ToastComponent = ({ title, icon, color }) => (
     <>
       <div className='toastify-header pb-0'>
@@ -151,7 +152,11 @@ const dynamic = () => {
             }    
         }, [pageData])
     return <>
-    <Breadcrumbs breadCrumbTitle='Manage Page' breadCrumbParent="page"  breadCrumbActive='Manage Page'/>
+    <Breadcrumbs breadCrumbTitle='Manage Page' breadCrumbParent="page"  breadCrumbActive='Manage Page'>
+    <BreadcrumbItem>
+          <Link to='#'> Home </Link>
+        </BreadcrumbItem>
+    </Breadcrumbs>
         <Card>
             <CardBody>
                 <Row>
